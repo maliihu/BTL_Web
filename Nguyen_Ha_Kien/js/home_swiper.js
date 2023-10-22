d231ee0899c5493eae24d702db08a80bf43b6ef4
kinatraa <hakien.237@gmail.com>
2023-10-22 21:01:04 +0700

    update responsive homepage

--- Nguyen_Ha_Kien/js/home_swiper.js
+++ Nguyen_Ha_Kien/js/home_swiper.js
@@ -13,14 +13,36 @@ const swiper = new Swiper('.swiper_main', {
 
 var swiper_film = new Swiper(".swiper_film", {
     slidesPerView: 4,
-    spaceBetween: 50,
+    spaceBetween: 40,
     loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    // speed: 2000,
-    // autoplay: {
-    //     delay: 4000,
-    // },
 });
+
+var breakpoints = [
+    { mediaQuery: "(max-width: 575px)", slidesPerView: 1, spaceBetween: 0},
+    { mediaQuery: "(max-width: 991px)", slidesPerView: 3, spaceBetween: 20},
+    { mediaQuery: "(max-width: 1199px)", slidesPerView: 4, spaceBetween: 20},
+];
+
+function res() {
+    var matched = false;
+    for (var i = 0; i < breakpoints.length; i++) {
+        if (window.matchMedia(breakpoints[i].mediaQuery).matches) {
+            swiper_film.params.slidesPerView = breakpoints[i].slidesPerView;
+            swiper_film.params.spaceBetween = breakpoints[i].spaceBetween;
+            swiper_film.update();
+            matched = true;
+            break; 
+        }
+    }
+    if (!matched) {
+        swiper_film.params.slidesPerView = 4;
+        swiper_film.params.spaceBetween = 50;
+        swiper_film.update();
+    }
+}
+res();
+window.addEventListener("resize", res);
