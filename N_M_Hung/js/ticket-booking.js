bb0ca4cf1a34ef24f4edb9750de7114984bdde18
14nmh <nguyenmanhhung20184@gmail.com>
2023-10-25 16:35:13 +0700

    1
    2

--- N_M_Hung/js/ticket-booking.js
+++ N_M_Hung/js/ticket-booking.js
@@ -1,4 +1,3 @@
-
 let seats = document.querySelector(".all-seats");
 for (var i = 0; i < 80; i++) {
   let randint = Math.floor(Math.random() * 4);
@@ -7,7 +6,7 @@ for (var i = 0; i < 80; i++) {
   inputElement.type = "checkbox";
   inputElement.name = "tickets";
   inputElement.id = "s" + (i + 2);
-  inputElement.disabled = booked === "booked"; 
+  inputElement.disabled = booked === "booked";
 
   let labelElement = document.createElement("label");
   labelElement.htmlFor = "s" + (i + 2);
@@ -16,7 +15,8 @@ for (var i = 0; i < 80; i++) {
   seats.appendChild(inputElement);
   seats.appendChild(labelElement);
 }
-var ticket_price = 10
+
+var ticket_price = 10;
 let tickets = seats.querySelectorAll("input");
 tickets.forEach((ticket) => {
   ticket.addEventListener("change", () => {
@@ -24,13 +24,16 @@ tickets.forEach((ticket) => {
     let count = document.querySelector(".count").innerHTML;
     amount = Number(amount);
     count = Number(count);
+
     if (!ticket.disabled) {
       if (ticket.checked) {
         count += 1;
         amount += ticket_price;
+        ticket.nextElementSibling.classList.add("selected-seat");
       } else {
         count -= 1;
         amount -= ticket_price;
+        ticket.nextElementSibling.classList.remove("selected-seat");
       }
     } else {
       ticket.checked = false;
@@ -40,3 +43,29 @@ tickets.forEach((ticket) => {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  var xacNhanButton = document.getElementById("xac-nhan-button");
+  xacNhanButton.addEventListener("click", function (event) {
+    var selectedSeats = document.querySelectorAll(".selected-seat");
+    if (selectedSeats.length === 0) {
+      event.preventDefault();
+      alert("Vui lòng chọn ghế!");
+    }
+  });
+});
+
+function resetPage() {
+  let tickets = document.querySelectorAll("input[type='checkbox']:checked");
+  tickets.forEach((ticket) => {
+    ticket.checked = false;
+  });
+  document.querySelector(".amount").innerHTML = "0";
+  document.querySelector(".count").innerHTML = "0";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var xacNhanButton = document.getElementById("xac-nhan-button");
+  xacNhanButton.addEventListener("click", function () {
+    resetPage();
+  });
+});
